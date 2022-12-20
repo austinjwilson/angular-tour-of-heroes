02a95ccdc6dcabeb8946df5d19c7f129683f05bd
Austin Wilson <austincodingmo@gmail.com>
2022-12-20 10:35:16 -0600

    Heroes Form

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,14 @@
-export interface Hero {
-  id: number;
-  name: string;
-}
+/** Code below for Tour of Heroes */
+// export interface Hero {
+//   id: number;
+//   name: string;
+// }
+/* Code below is for the Hero Form*/
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string,
+    public power: string,
+    public alterEgo?: string
+  ) { }
+} 
